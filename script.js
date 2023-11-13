1aa38328746e99a22480603241e47a392c37f8a9
astrange90 <astrange90@hotmail.com>
2023-11-12 21:24:38 -0800

    added user story and acceptance criteria to README

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 fetch (queryURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+});
 
 
